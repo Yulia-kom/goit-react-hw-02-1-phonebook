321c56a8d9c4c9e50d2f5e07266f76eeb649cf9c
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-07-05 22:18:03 +0300

    add settings1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ContactForm from '../components/ContactForm';
 import ContactList from '../components/ContactList';
 import Filter from '../components/Filter';
 
-const App = () => {
- 
-  render() {
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
 
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
-    ); 
-  }  
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
